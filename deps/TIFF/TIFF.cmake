1250c042745ab1f5a779fb3d4d9a7b9a127682be
PJarczak <paweljarczak@int.pl>
2026-03-03 15:39:30 +0100

    CI: replace hashFiles deps key with git tree hash

--- deps/TIFF/TIFF.cmake
+++ deps/TIFF/TIFF.cmake
@@ -1,8 +1,8 @@
 find_package(OpenGL QUIET REQUIRED)
 
 bambustudio_add_cmake_project(TIFF
-    URL https://gitlab.com/libtiff/libtiff/-/archive/v4.3.0/libtiff-v4.3.0.zip
-    URL_HASH SHA256=455abecf8fba9754b80f8eff01c3ef5b24a3872ffce58337a59cba38029f0eca
+    URL https://download.osgeo.org/libtiff/tiff-4.1.0.zip
+    URL_HASH SHA256=6F3DBED9D2ECFED33C7192B5C01884078970657FA21B4AD28E3CDF3438EB2419
     DEPENDS ${ZLIB_PKG} ${PNG_PKG} ${JPEG_PKG}
     CMAKE_ARGS
         -Dlzma:BOOL=OFF
@@ -10,8 +10,4 @@ bambustudio_add_cmake_project(TIFF
         -Djbig:BOOL=OFF
         -Dzstd:BOOL=OFF
         -Dpixarlog:BOOL=OFF
-        -Dtiff-tools:BOOL=OFF  # GLUT OFF
-        -Dtiff-tests:BOOL=OFF  # GLUT OFF
-        -DCMAKE_DISABLE_FIND_PACKAGE_GLUT=ON  # GLUT OFF <--- w 4.3.0 to bedzie to chyba
-        -DCMAKE_DISABLE_FIND_PACKAGE_OpenGL=ON # <--- też nie potrzebne
 )
