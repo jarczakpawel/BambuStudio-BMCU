f63caed21f4cb0e35ca7684b005b90984742adc1
PJarczak <paweljarczak@int.pl>
2026-04-03 20:07:44 +0200

    Fix linux bridge config build

--- src/slic3r/Utils/PJarczakLinuxBridge/PJarczakLinuxBridgeConfig.cpp
+++ src/slic3r/Utils/PJarczakLinuxBridge/PJarczakLinuxBridgeConfig.cpp
@@ -6,8 +6,10 @@
 #include <sstream>
 #include <iomanip>
 #include <cstdlib>
+#include <boost/filesystem/operations.hpp>
 #include <openssl/sha.h>
 #include <nlohmann/json.hpp>
+#include "../bambu_networking.hpp"
 
 namespace Slic3r::PJarczakLinuxBridge {
 
@@ -216,7 +218,7 @@ std::string sha256_file_hex(const std::string& file_path, std::string* reason)
 
 std::string expected_network_abi_version()
 {
-    return env_or("PJARCZAK_EXPECTED_BAMBU_NETWORK_VERSION", BBL::BAMBU_NETWORK_AGENT_VERSION);
+    return env_or("PJARCZAK_EXPECTED_BAMBU_NETWORK_VERSION", BAMBU_NETWORK_AGENT_VERSION);
 }
 
 bool abi_version_matches_expected(const std::string& actual_version, std::string* reason)
